9b62f1125032096bf0246bbc640881e944626df2
JMikoenig <javi.mik.nao@gmail.com>
2024-08-22 14:05:43 +0200

    fixed push.c

--- push.c
+++ push.c
@@ -8,8 +8,8 @@ void	push_a(Node **head_a, Node **head_b)
 	*head_a = current->next;
 	current->prev->next = current->next;
 	current->next->prev = current->prev;
-	current->next = &head_b;
-	current->prev = &head_b->prev;
+	current->next = *head_b;
+	current->prev = (*head_b)->prev;
 	current->next->prev = current;
 	current->prev->next = current;
 	*head_b = current;
@@ -23,8 +23,8 @@ void	push_b(Node **head_a, Node **head_b)
 	*head_b = current->next;
 	current->prev->next = current->next;
 	current->next->prev = current->prev;
-	current->next = &head_a;
-	current->prev = &head_a->prev;
+	current->next = *head_a;
+	current->prev = (*head_a)->prev;
 	current->next->prev = current;
 	current->prev->next = current;
 	*head_a = current;
