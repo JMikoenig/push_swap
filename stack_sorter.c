7a9d01464109536c5841dcc617989918d2d70af0
JMikoenig <55178928+JMikoenig@users.noreply.github.com>
2024-08-27 15:10:09 +0200

    Update stack_sorter.c
    COMPROBAR LA SINTAXIS DE LOS POINTERS DE LOS INPUTS
    
    - sort()
    1. hay un while que sigue ejecutando comandos hasta que el ultimo comando devuelva 0; creo que se puede cambiar esa condicion por size_a > 3 y ademas asi eliminamos una linea, quedando dentro del limite de 25 (el printf sube las lineas a 27 pero como lo vamos a quitar, no lo he contado)
    2. cuando size_a > 3 deberia parar y ejecutarse el sort_three (queda pendiente gestionar si el tamaño inicial era 4, tal y como esta quedarian 2 stacks de tamaño 2)
    
    - get_next_command()
    añadi un int command cuyo valor depende del comando a ejecutar (gracias a perform_next_command()), que entiendo es lo que queriamos, y ahora devolvemos eso en vez de 0
    
    - perform_next_command()
    1. tambien tiene un int command, llamando a select_command() le doy un valor y ahora lo uso para decidir que comando lanzar
    2. antes de ejecutar ningun comando, si el size_a == 3 devuelve 0 porque aqui habria que irse al sort_three(), pero no me cabe aqui llamar a esa funcion (aunque podria devolver un valor especifico que signifique "ejecutar sort_three()")
    3. como dice el comentario, la funcion tiene 25 lineas exactas; si decidimos imprimir desde aqui, puede hacerse desde el return
    
    - select_command()
    1. si el nodo elegido y su target estan en las cabezas de los stacks, hace push, y si no decide que comando ejecutar en base a los above_median del nodo y su target

--- stack_sorter.c
+++ stack_sorter.c
@@ -13,13 +13,73 @@
 #include "push_swap.h"
 #include <stdio.h>
 
+static int	select_command(t_node *node)
+{
+	if (!(node->pos) && (!node->target->pos)
+		return (2);
+	else if (node->above_median && node->target->above_median)
+		return (9);
+	else if (!(node->above_median) && (!node->target->above_median))
+		return (12);
+	else if (node->pos)
+	{
+		if (node->above_median)
+			return (7);
+		else
+			return (10);
+	}
+	else if (node->target->pos)
+	{
+		if (node->target->above_median)
+			return (8);
+		else
+			return (11);
+	}
+	return (0);
+}
+
+// justo 25 lineas; si hace falta el return puede ser una funcion "print_command(command)"
+// que recibe "command" como input, en base a eso imprime lo que corresponda,
+// y a su vez devuelva el input tal cual
+static int	perform_next_command(t_node *node, t_node **stack_a,
+	t_node **stack_b, int *size_a, int *size_b)
+{
+	int	command;
+
+	command = select_command(node);
+	if (*size_a == 3)
+			return (0);
+	if (command == 2)
+	{
+		push(*stack_a, *stack_b);
+		(*size_a)--;
+		(*size_b)++;
+	}
+	else if (command == 7)
+		rotate(stack_a);
+	else if (command == 8)
+		rotate(stack_b);
+	else if (command == 9)
+		double_rotate(*stack_a, *stack_b);
+	else if (command == 10)
+		rev_rotate(stack_a);
+	else if (command == 11)
+		rev_rotate(stack_b);
+	else if (command == 12)
+		double_rev_rotate(*stack_a, *stack_b);
+	}
+	return (command);
+}
+
 static int	get_next_command(t_node **stack_a, t_node **stack_b,
 	int *size_a, int *size_b)
 {
 	t_node	*selected_node;
-	selected_node = get_selected_node(*stack_a, *stack_b, *size_a, *size_b);
+	int	command;
 
-	return (0);
+	selected_node = get_selected_node(*stack_a, *stack_b, *size_a, *size_b);
+	command = perform_next_command(t_node selected_node, *stack_a, *stack_b, size_a, size_b);
+	return (command);
 }
 
 int	sort(const int *input_arr, int size)
@@ -45,7 +105,7 @@ int	sort(const int *input_arr, int size)
 		(size_b)++;
 		printf("2\n");
 	}
-	while (latest_command)
+	while (latest_command)	// size_a > 3 y nos olvidamos de latest_command?
 	{
 		set_nodes(stack_a, size_a, stack_b, size_b);
 		latest_command = get_next_command(&stack_a, &stack_b, &size_a, &size_b);
