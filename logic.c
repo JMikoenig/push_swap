c87fa55215bfcbb2d773244c838314f11f16f02b
JMikoenig <55178928+JMikoenig@users.noreply.github.com>
2024-08-27 13:29:43 +0200

    updated logic.c
    - he cambiado cheapest de stack_a->value a INT_MAX y i = 1 a i = 0 porque en cheapest lo que queremos es el menor coste, no el menor valor, y por tanto hay que comprobar todos los nodos (con i = 1 nos saltabamos el primero)
    - por lo mismo he movido el current = current->next al final del bucle, justo antes del i++ (el resultado es el mismo, pero es mas legible asi)
    - he borrado lo que parecian ser parametros sueltos de algun printf que hubiera antes, y otras lineas con printf

--- logic.c
+++ logic.c
@@ -10,26 +10,19 @@ t_node	*get_selected_node(t_node *stack_a, const t_node *stack_b,  int size_a, i
 	t_node	*current;
 	t_node	*cheapest_node;
 
-	i = 1;
+	i = 0;
 	cost = 0;
-	printf("size_a: %d\n", size_a);
-	printf("size_b: %d\n", size_b);
-	cheapest = stack_a->value;
+	cheapest = INT_MAX;
 	current = stack_a;
 	while (i < size_a)
 	{
-		current = current->next;
-		// printf("get_selected_node >> get_cost >> current: %d, size_a: %d, size_b: %d\n", current->value, size_a, size_b);
-		printf("get_selected_node >> get_cost >> current: %d, target: %d, size_a: %d, size_b: %d\n", current->value, current->target->value, size_a, size_b);
 		cost = get_cost(current, size_a, size_b);
-			current->value,
-			current->pos,
-			cost;
 		if (cost < cheapest)
 		{
 			cheapest = cost;
 			cheapest_node = current;
 		}
+		current = current->next;
 		i++;
 
 	}
