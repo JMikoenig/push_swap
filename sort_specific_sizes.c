f78671a9cdef5e607d4fa1c2130348843f3d5520
Javier <javi.mik.nao@gmail.com>
2024-09-09 14:38:56 +0200

    norm checks

--- sort_specific_sizes.c
+++ sort_specific_sizes.c
@@ -6,7 +6,7 @@
 /*   By: jamanzan <jamanzan@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 18:27:55 by jamanzan          #+#    #+#             */
-/*   Updated: 2024/09/09 11:15:01 by jamanzan         ###   ########.fr       */
+/*   Updated: 2024/09/09 14:38:04 by jamanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,6 @@ static void	sort_two(t_node **a)
 	return ;
 }
 
-// busca el nodo con el mayor valor
-// 1. si el mayor es la cabeza, mandalo al final
-	// 3 1 2 >> 1 2 3	DONE
-	// 3 2 1 >> 2 1 3	ONE MORE STEP
-// 2. si el mayor es el segundo nodo, mandalo al final
-	// 1 3 2 >> 2 1 3	ONE MORE STEP
-	// 2 3 1 >> 1 2 3	DONE
-// 3. el mayor esta al final si o si;
-// 	si el primero es mayor que el segundo, swap()
-	// 2 1 3 >> 1 2 3	DONE
 void	sort_three(t_node **a)
 {
 	t_node	*highest;
@@ -53,8 +43,4 @@ void	short_sort(t_node **a, int size)
 		sort_two(a);
 	if (size == 3)
 		sort_three(a);
-	// if (size == 4)
-	// 	sort_four();
-	// if (size == 5)
-	// 	sort_five();
 }
